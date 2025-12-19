8a26d0f854b83b018fd97c67181b46430872cb51
Ruf1ko606 <ruf1k@inbox.ru>
2025-12-19 14:14:47 +0300

    fix n2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-typescript: { ignoreBuildErrors: true }
-export default nextConfig;
-
+const nextConfig = {
+    // ВОТ ЗДЕСЬ, внутри скобок!
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
